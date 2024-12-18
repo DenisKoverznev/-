2908d8bbdf3ddfaa1d097e3813b29ea953377268
DenisKoverznev <dionisk2006@gmail.com>
2024-12-18 21:10:01 +0200

    Update app.h

--- products store/include/app.h
+++ products store/include/app.h
@@ -2,17 +2,17 @@
 
 #include <stdio.h>
 
+
 struct Product {
-	char name[128];
-	int price ;
-	int expire;
+    char name[128];  
+    int price;       
+    int expire;      
 };
 
-typedef struct Product PRODUCT;
-typedef struct Product*  PPRODUCT;
+typedef struct Product PRODUCT;     
+typedef struct Product* PPRODUCT;  
 
+void print(PRODUCT* product);
 
-
-void print(PPRODUCT product);
 
 void print_array(PRODUCT product[], int size);
