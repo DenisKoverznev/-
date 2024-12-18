226bc7fd83b3ca4d57c452466b4e83dfe5a4bc28
DenisKoverznev <dionisk2006@gmail.com>
2024-12-18 20:47:18 +0200

    Update app.c

--- products store/src/app.c
+++ products store/src/app.c
@@ -1,14 +1,13 @@
 #include <app.h>
-
 void print(PRODUCT* product) {
+        printf("name = %s\n", product->name);
+        printf("price = %d\n", product->price);
+        printf("expire = %d\n", product->expire);
+}
     
-        printf("name = %s\n", product[i].name);
-        printf("price = %d\n", product[i].price);
-        printf("expire = %d\n", product[i].expire);
-    }
-    void print_array(PRODUCT product[], int size) {
+void print_array(PRODUCT product[], int size) {
         for (int i = 0; i < size; i++) {
             print("Product %d:\n", i + 1);
             print(&product[i]);
         }
-    }
+}
