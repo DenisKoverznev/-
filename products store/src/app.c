75767f6c951d92b37ad55410e4fda76fd2be1076
DenisKoverznev <dionisk2006@gmail.com>
2024-12-18 20:33:32 +0200

    Update app.c

--- products store/src/app.c
+++ products store/src/app.c
@@ -1,26 +1,14 @@
-#include "app.h"
+#include <app.h>
 
-void print_array(PRODUCT product[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("Product %d:\n", i + 1);
-        printf("  name = %s\n", product[i].name);
-        printf("  price = %d\n", product[i].price);
-        printf("  expire = %d\n", product[i].expire);
+void print(PRODUCT* product) {
+    
+        printf("name = %s\n", product[i].name);
+        printf("price = %d\n", product[i].price);
+        printf("expire = %d\n", product[i].expire);
     }
-}
-
-int main() {
-    PRODUCT product1 = { "Apple", 100, 30 };
-    PRODUCT product2 = { "Banana", 200, 15 };
-
-    PRODUCT products[] = { product1, product2 };
-    int size = sizeof(products) / sizeof(products[0]);
-
-    // Вывод одного продукта
-    print(&product1);
-
-    // Вывод массива продуктов
-    print_array(products, size);
-
-    return 0;
-}
+    void print_array(PRODUCT product[], int size) {
+        for (int i = 0; i < size; i++) {
+            print("Product %d:\n", i + 1);
+            print(&product[i]);
+        }
+    }
